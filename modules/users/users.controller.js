9c10a12818c3211d7542ff48da58ec0865c1601a
sami157 <sami.eee157@gmail.com>
2025-12-09 12:55:05 +0600

    code cleanup

--- modules/users/users.controller.js
+++ modules/users/users.controller.js
@@ -1,7 +1,7 @@
 exports.createUser = async (req, res) => {
   const { name, email, role } = req.body 
   try {
-    console.log('User Created')
+    res.send('user created')
   } catch (error) {
     console.error('Error creating user:', error);
     res.status(500).json({ message: 'Server error, please try again later' });
